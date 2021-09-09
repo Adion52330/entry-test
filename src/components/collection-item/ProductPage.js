c11ec833982e2cce3dc5d1cf2c40d60a993551c6
Koranteng Anane <squidiw.squidiw@gmail.com>
2021-09-09 16:36:31 +0000

    created redux code for addItemToCart 1.0.0

--- src/components/collection-item/ProductPage.js
+++ src/components/collection-item/ProductPage.js
@@ -1,43 +1,65 @@
 import { graphql } from "@apollo/client/react/hoc";
 import React, { Component } from "react";
+import { connect } from "react-redux";
+
 import { getCategory } from "../../graphql/queries";
 import { Link } from "react-router-dom";
+import { addItem } from "../../redux/cart/cart.actions";
 
 import "./collection-item.styles.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
 class ProductPage extends Component {
    render(props) {
       console.log(this.props.data);
+      const { addItem } = this.props;
       return (
-         <div>
-            <div className="collection-wrapper">
-               {this.props.data.category?.products?.map((p, index) => (
-                     <div className="collection-item" key={index}>
-                        <Link className="image" to={`/${p.category}/${p.id}`}>
-                           <img
-                              className="image"
-                              style={{
-                                 backgroundImage: `url("${p.gallery[0]}")`,
-                              }}
-                              alt=""
-                           />
+         <div className="category-page">
+            <div className="category">
+               <div className="category__title">
+                  <h1>{this.props.data?.category?.name}</h1>
+               </div>
+               <div className="category__body">
+                  {this.props.data?.category?.products?.map((p, index) => (
+                     <div className="box" key={index}>
+                        <Link to={`/${p.category}/${p.id}`}>
+                           <div className="image-container">
+                              <img
+                              className='image'
+                                 style={{
+                                    backgroundImage: `url("${p.gallery[0]}")`,
+                                 }}
+                                 alt=""
+                              />
+                           </div>
                         </Link>
-                        <div className="collection-footer">
-                           <span className="name">{p.name}</span>
-                           <span className="price">
+                        <div className="product-info">
+                           <p className="product-name">{p.name}</p>
+                           <h3 className="price">
                               {p.prices[0].currency} {p.prices[0].amount}
-                           </span>
+                           </h3>
+                           <div className="show-cart-icon" onClick={() => addItem(addItem)}>
+                           <FontAwesomeIcon icon={faShoppingCart}/>
+                        </div>
                         </div>
                      </div>
-               ))}
+                  ))}
+               </div>
             </div>
          </div>
       );
    }
 }
 
-export default graphql(getCategory, {
+const mapDispatchToProps = (dispatch) => ({
+   addItem: item => dispatch(addItem(item))
+})
+
+const graph = graphql(getCategory, {
    options: (props) => ({
       variables: { input: { title: props.match.params.category } },
    }),
 })(ProductPage);
+
+export default connect(null, mapDispatchToProps)(graph)
