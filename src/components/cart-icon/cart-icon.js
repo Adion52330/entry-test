21dc1db815eba5ba4d9ff039f0dde51c5312078f
Koranteng Anane <squidiw.squidiw@gmail.com>
2021-09-19 10:06:39 +0000

    added reselectors to application, replaced cart selectors with memoized selectors

--- src/components/cart-icon/cart-icon.js
+++ src/components/cart-icon/cart-icon.js
@@ -1,29 +1,29 @@
 import React from "react";
 import { connect } from "react-redux";
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
+import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-cart-svgrepo-com.svg";
 
 import "./cart-icon.styles.scss";
 
 const CartIcon = ({ toggleCartHidden, itemCount }) => {
-   return <div className="cart-icon" onClick={toggleCartHidden}>
-      <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">{itemCount}</span>
-   </div>;
+   return (
+      <div className="cart-icon" onClick={toggleCartHidden}>
+         <ShoppingIcon className="shopping-icon" />
+         <span className="item-count">{itemCount}</span>
+      </div>
+   );
 };
 
-const mapDispatchToProps =  dispatch => ({
-  toggleCartHidden: () => dispatch(toggleCartHidden())
-})
+const mapDispatchToProps = (dispatch) => ({
+   toggleCartHidden: () => dispatch(toggleCartHidden()),
+});
 
 //SELECTOR
-const mapStateToProps = ({ cart: {cartItems} }) => ({
-  itemCount: cartItems.reduce( (accumulatedQuantity,cartItem) => accumulatedQuantity + cartItem.quantity, 0)
-})
+const mapStateToProps = (state) => ({
+   itemCount: selectCartItemsCount(state),
+});
 // ARROW FUNCTION ACCUMULATES ALL THE NUMBER VALUES OF THE QUANTITY ON ALL CARTITEMS.
 // 0 IS INITIAL ACCUMULATED VALUE
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(CartIcon)
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
